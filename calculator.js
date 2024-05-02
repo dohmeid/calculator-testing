078b4da7b116dadef24457bd23cde38e49ae9bfe
dohmeid <doha.hmeid@gmail.com>
2024-05-02 11:00:22 +0300

    enhancments on calculator.js

--- calculator.js
+++ calculator.js
@@ -1,37 +1,32 @@
 //the calculator function
 const calc = (...args) => {
-
   //the number of arguments passed must be at least 3 (num1,op,num2), also, for multiple operations, make sure the args are odd number
   if (args.length < 3 || args.length % 2 === 0) {
     throw new Error("Invalid input");
   }
 
+  const ops = "+-*/"; //the valid operationa
   const operandStack = []; //Stack for numbers/operands
   const operatorStack = []; //Stack for Operators
-  const ops = "+-*/";
 
   for (let i = 0; i < args.length; i++) {
-
     //current token is a number, push to operands stack
     if (typeof args[i] === "number") {
       let operand = parseFloat(args[i]);
       operandStack.push(operand);
     }
 
-    //current token is an operator, , push to operators stack
+    //current token is an operator, push to operators stack
     else if (ops.includes(args[i])) {
-      let operation = args[i];
+      let currOp = args[i];
+      let prevOp = operatorStack[operatorStack.length - 1];
 
-      while (operatorStack.length &&hasPrecedence(operation, operatorStack[operatorStack.length - 1])
-      ) {
-        let op = operatorStack.pop();
-        let num2 = operandStack.pop();
-        let num1 = operandStack.pop();
-        let res = apply(op, num1, num2);
+      //as long as prev op has same or greater precedence to curr op -> apply op prev to top two elements in operandStack
+      while (operatorStack.length && hasPrecedence(currOp, prevOp)) {
+        let res = applyOperation();
         operandStack.push(res);
       }
-
-      operatorStack.push(operation);
+      operatorStack.push(currOp);
     }
 
     //if the expected operand token is not a number
@@ -45,17 +40,16 @@ const calc = (...args) => {
     }
   }
 
+  //apply remaining ops to remaining values
   while (operatorStack.length) {
-    let op = operatorStack.pop();
-    let num2 = operandStack.pop();
-    let num1 = operandStack.pop();
-    let res = apply(op, num1, num2);
+    let res = applyOperation();
     operandStack.push(res);
   }
 
   return operandStack.pop();
-}
+};
 
+//this function checks if op2 has higher or same precedence as op1
 const hasPrecedence = (op1, op2) => {
   if ((op1 === "*" || op1 === "/") && (op2 === "+" || op2 === "-")) {
     return false;
@@ -63,7 +57,12 @@ const hasPrecedence = (op1, op2) => {
   return true;
 };
 
-const apply = (op, num1, num2) => {
+//this function applies an operator from operatorStack on top 2 operands from operandStack and returns the result
+const applyOperation = () => {
+  let op = operatorStack.pop();
+  let num2 = operandStack.pop();
+  let num1 = operandStack.pop();
+
   switch (op) {
     case "+":
       return num1 + num2;
